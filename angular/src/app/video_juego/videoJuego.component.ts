232f63f848fe6e5a553b7841944d67aee1cbdc7d
DNavarro10 <luis_d_100@hotmail.com>
2019-06-24 23:17:11 -0600

    componentes y bootstrap

--- angular/src/app/video_juego/videoJuego.component.ts
+++ angular/src/app/video_juego/videoJuego.component.ts
@@ -2,13 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'video_juego',
-    templateUrl: './videoJuegocomponente.component.html',
-    styleUrls: ['./name.component.scss']
+    templateUrl: './videoJuego.component.html',
+    styleUrls: ['./videoJuego.component.scss']
 })
 export class VideoJuegoComponent implements OnInit {
+    //propiedades
+    public titulo: string;
+    public listado: string;
     constructor() {
+        this.titulo = "Titulo exportado desde el componente";
+        this.listado = "Listado ficticio";
+        //se pueden imprimir polarizando {{}}
         console.log('cargado el componente');
-     }
+    }
 
     ngOnInit(): void { }
 }
